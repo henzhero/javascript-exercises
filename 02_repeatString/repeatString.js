aafae97c6abe56c982dbc3f81e9e9838225a1fce
henzhero <189972177+henzhero@users.noreply.github.com>
2025-01-30 19:46:02 +0800

    passed the test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    let stringContainer = "";
+
+    for (let i = 0; i < num; i++) {
+        stringContainer += string;
+    }
+
+    return stringContainer;
 };
 
 // Do not edit below this line
