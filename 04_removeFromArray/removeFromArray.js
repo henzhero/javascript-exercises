de7291b7c88952f94cc17336cd7254647726c9ff
henzhero <189972177+henzhero@users.noreply.github.com>
2025-01-30 20:55:43 +0800

    test passed. found a succinct way using method and anonymous callback function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+
+    return array.filter(element => element != remove);
+    
 };
 
 // Do not edit below this line
